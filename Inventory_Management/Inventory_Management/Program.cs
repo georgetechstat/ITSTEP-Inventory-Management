905b59fcc0572f679782cb30f30bfff9ead579a9
georgetechstat <georgeparun@outlook.com>
2024-03-20 16:28:11 +0400

    Added comments, action cases.

--- Inventory_Management/Inventory_Management/Program.cs
+++ Inventory_Management/Inventory_Management/Program.cs
@@ -8,6 +8,10 @@ namespace Inventory_Management
 {
     internal class Program
     {
+        /// <summary>
+        /// Recursive function that gets the (int) input safely 
+        /// </summary>
+        /// <returns>returns (int)[1-6]</returns>
         public static int GetUserAction()
         {
             string input = Console.ReadLine();
@@ -30,6 +34,10 @@ namespace Inventory_Management
                 return GetUserAction();
             }
         }
+        /// <summary>
+        /// Gets the user input of all types (Unparsed)
+        /// </summary>
+        /// <returns>User input (string)</returns>
         public static string GetUserInput()
         {
             return Console.ReadLine();
@@ -49,7 +57,7 @@ namespace Inventory_Management
                 "Select an operation:\n" +
                 "1. Add a product\n" +
                 "2. Remove a product\n" +
-                "3. Process the order\n" +
+                "3. Export the order\n" +
                 "4. Search product\n" +
                 "5. View inventory statistics\n" +
                 "6. Exit";
@@ -62,9 +70,29 @@ namespace Inventory_Management
                 Console.WriteLine(actionPrompt);
 
                 userActionBuffer = GetUserAction();
-                if (userActionBuffer == 6) break;
 
-
+                switch (userActionBuffer)
+                {
+                    case 6:
+                        // Exit
+                        run = false;
+                        break;
+                    case 1:
+                        // Add a product
+                        break;
+                    case 2:
+                        // Remove a product
+                        break;
+                    case 3:
+                        // Export the order
+                        break;
+                    case 4:
+                        // Search product
+                        break;
+                    case 5:
+                        // Show inventory statistics
+                        break;
+                }
             }
         }
     }
