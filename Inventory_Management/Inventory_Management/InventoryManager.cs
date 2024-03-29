8d3ee02c219109f3a7f481d85f89e67febfe53f9
George <117456377+georgetechstat@users.noreply.github.com>
2024-03-29 22:51:23 +0400

    Added product search system based on filters.

--- Inventory_Management/Inventory_Management/InventoryManager.cs
+++ Inventory_Management/Inventory_Management/InventoryManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,47 +12,17 @@ namespace Inventory_Management
     public class InventoryManager
     {
         public List<Product> Products { get; set; }
-        public void AddProduct(Product product)
-        {
-            Products.Add(product);
-        }
-        public bool RemoveProduct(string productId)
-        {
-            return false;
-        }
-        public void DisplayInventory()
-        {
-            Products.ForEach(product => { Console.WriteLine(product.ToString()); });
-        }
-        public void ExportOrder()
-        {
+        public List<string> Categories { get; set; }
 
-        }
-        public Product SearchProductByName(string productName)
+        public InventoryManager()
         {
-            return null;
-        }
-        public List<Product> SearchProductByCategory(string category)
-        {
-            return null;
-        }
-        public int GetTotalProducts()
-        {
-            return Products.Count;
-        }
-        /// <summary>
-        /// Sum up all prices of products.
-        /// Sum = foreach (p in ps) { sum(p.Price * p.Qty) } return sum;
-        /// </summary>
-        /// <returns></returns>
-        public double GetTotalHolding()
-        {
-            double totalHolding = 0;
-            foreach (Product p in Products)
+            Products = new List<Product>();
+            Categories = new List<string>()
             {
-                totalHolding += p.Price * p.Quantity;
-            }
-            return totalHolding;
+                "Furniture",
+                "Tool",
+                "Electric"
+            };
         }
         public void SerializeInventory()
         {
--- Inventory_Management/Inventory_Management/Program.cs
+++ Inventory_Management/Inventory_Management/Program.cs
@@ -9,49 +9,44 @@ namespace Inventory_Management
     internal class Program
     {
         /// <summary>
-        /// Recursive function that gets the (int) input safely 
+        /// Recursive function for getting user input in range of (inclusive) lower and upper
         /// </summary>
-        /// <returns>returns (int)[1-6]</returns>
-        public static int GetUserAction()
+        public static int GetUserAction(int lower, int upper)
         {
-            string input = Console.ReadLine();
-            int k = -1;
+            int choice = int.Parse(Console.ReadLine());
+            bool pass = true;
 
-            try
+            if (choice < lower || choice > upper)
             {
-                k = int.Parse(input);
-                if (k < 1 || k > 6) throw new OverflowException();
-                return k;
+                Console.WriteLine($"The input must be in range [{lower} : {upper}]");
+                pass = false;
             }
-            catch (FormatException)
+
+            if (pass)
             {
-                Console.WriteLine("The action input must be an integer [1-6]");
-                return GetUserAction();
+                return choice;
             }
-            catch (OverflowException)
+            else
             {
-                Console.WriteLine("The action input must be an integer between 1 and 6 (inclusive)");
-                return GetUserAction();
+                return GetUserAction(lower, upper);
             }
         }
-        /// <summary>
-        /// Gets the user input of all types (Unparsed)
-        /// </summary>
-        /// <returns>User input (string)</returns>
-        public static string GetUserInput()
+        public static void PrintDataOnFilters(List<Product> current, List<string> filters)
         {
-            return Console.ReadLine();
+            Console.Write("Showing products with the filter(s):");
+            foreach (string filter in filters)
+            {
+                Console.Write($"{filter}\n");
+            }
+
+            foreach (Product prod in current)
+            {
+                Console.Write(prod.ToString());
+            }
         }
         static void Main(string[] args)
         {
-            /*
-             * TODO
-             * 1. The ability to list multiple product ids to remove:
-             *    ~ id1
-             *    ~ id2
-             *    ~ id3
-             *    ~ (some keyword to finish listing ids)
-            */
+            InventoryManager inventoryManager = new InventoryManager();
 
             string actionPrompt =
                 "Select an operation:\n" +
@@ -62,36 +57,158 @@ namespace Inventory_Management
                 "5. View inventory statistics\n" +
                 "6. Exit";
 
+            int UserAction = 0;
             bool run = true;
-            int userActionBuffer = 0;
 
+            // main loop
             while (run)
             {
                 Console.WriteLine(actionPrompt);
+                UserAction = GetUserAction(1, 6);
 
-                userActionBuffer = GetUserAction();
-
-                switch (userActionBuffer)
+                if (UserAction == 6)
                 {
-                    case 6:
-                        // Exit
-                        run = false;
-                        break;
-                    case 1:
-                        // Add a product
-                        break;
-                    case 2:
-                        // Remove a product
-                        break;
-                    case 3:
-                        // Export the order
-                        break;
-                    case 4:
-                        // Search product
-                        break;
-                    case 5:
-                        // Show inventory statistics
-                        break;
+                    run = false;
+                    break;
+                }
+                else if (UserAction == 1)
+                {
+                    // Add a product
+                }
+                else if (UserAction == 2)
+                {
+                    // Remove a product
+                }
+                else if (UserAction == 3)
+                {
+                    // Export the order
+                }
+                else if (UserAction == 4)
+                {
+                    // Search Product
+                    bool Task_Search = true;
+                    List<string> Filters = new List<string>();
+                    List<Product> current = inventoryManager.Products;
+
+                    while (Task_Search)
+                    {
+                        Console.Write("Search by:\n" +
+                            "1. Product Name\n" +
+                            "2. Product Category\n" +
+                            "3. Product Manufacturer\n" +
+                            "4. Product Quantity\n" +
+                            "5. Product Price\n" +
+                            "6. Exit\n" +
+                            "Your Prompt: ");
+
+                        UserAction = GetUserAction(1, 6);
+
+                        switch (UserAction)
+                        {
+                            case 6:
+                                Task_Search = false;
+                                break;
+                            case 1:
+                                // by name
+                                Console.Write("Enter the product's name: ");
+                                string prodname = Console.ReadLine();
+
+                                current = current.Where(p => p.Name == prodname).ToList();
+                                Filters.Add($"Name: {prodname}");
+
+                                Console.Write("Showing products with the filter(s):");
+                                foreach (string filter in Filters)
+                                {
+                                    Console.Write($"{filter}\n");
+                                }
+
+                                foreach (Product prod in current)
+                                {
+                                    Console.Write(prod.ToString());
+                                }
+                                break;
+                            case 2:
+                                // by category
+                                Console.Write("Enter the product's category: ");
+                                string prodcategory = Console.ReadLine();
+
+                                current = current.Where(p => p.Category == prodcategory).ToList();
+                                Filters.Add($"Category: {prodcategory}");
+
+                                Console.Write("Showing products with the filter(s):");
+                                foreach (string filter in Filters)
+                                {
+                                    Console.Write($"{filter}\n");
+                                }
+
+                                foreach (Product prod in current)
+                                {
+                                    Console.Write(prod.ToString());
+                                }
+                                break;
+                            case 3:
+                                // by manufacturer
+                                Console.Write("Enter the product's manufacturer: ");
+                                string prodmanufacturer = Console.ReadLine();
+
+                                current = current.Where(p => p.Manufacturer == prodmanufacturer).ToList();
+                                Filters.Add($"Manufacturer: {prodmanufacturer}");
+
+                                Console.Write("Showing products with the filter(s):");
+                                foreach (string filter in Filters)
+                                {
+                                    Console.Write($"{filter}\n");
+                                }
+
+                                foreach (Product prod in current)
+                                {
+                                    Console.Write(prod.ToString());
+                                }
+                                break;
+                            case 4:
+                                // by qty
+                                Console.Write("Enter the product's quantity: ");
+                                int prodqty = GetUserAction(0, int.MaxValue);
+
+                                current = current.Where(p => p.Quantity == prodqty).ToList();
+                                Filters.Add($"Quantity: {prodqty}");
+
+                                Console.Write("Showing products with the filter(s):");
+                                foreach (string filter in Filters)
+                                {
+                                    Console.Write($"{filter}\n");
+                                }
+
+                                foreach (Product prod in current)
+                                {
+                                    Console.Write(prod.ToString());
+                                }
+                                break;
+                            case 5:
+                                // by price
+                                Console.Write("Enter the product's price: ");
+                                double prodprice = double.Parse(Console.ReadLine());
+
+                                current = current.Where(p => p.Price == prodprice).ToList();
+                                Filters.Add($"Price: {prodprice}");
+
+                                Console.Write("Showing products with the filter(s):");
+                                foreach (string filter in Filters)
+                                {
+                                    Console.Write($"{filter}\n");
+                                }
+
+                                foreach (Product prod in current)
+                                {
+                                    Console.Write(prod.ToString());
+                                }
+                                break;
+                        }
+                    }
+                }
+                else if (UserAction == 5)
+                {
+                    // View inventory statistics
                 }
             }
         }
